f904478e2e05cd2d43b8c0763a198e6219d5e25f
NOBITA <88440061+Nobita-o@users.noreply.github.com>
2021-08-18 03:06:29 +0600

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -1,10 +1,10 @@
 export MAX_DOWNLOAD_SPEED=0
 tracker_list=$(curl -Ns https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt https://ngosang.github.io/trackerslist/trackers_all_http.txt https://newtrackon.com/api/all https://raw.githubusercontent.com/DeSireFire/animeTrackerList/master/AT_all.txt https://raw.githubusercontent.com/hezhijie0327/Trackerslist/main/trackerslist_tracker.txt https://raw.githubusercontent.com/hezhijie0327/Trackerslist/main/trackerslist_exclude.txt | awk '$0' | tr '\n' ',')
-export MAX_CONCURRENT_DOWNLOADS=7
+export MAX_CONCURRENT_DOWNLOADS=5
 
 aria2c --enable-rpc --rpc-listen-all=false --check-certificate=false \
    --max-connection-per-server=10 --rpc-max-request-size=1024M \
-   --bt-tracker="[$tracker_list]" --bt-max-peers=0 --bt-tracker-connect-timeout=300 --bt-stop-timeout=1200 --min-split-size=10M \
+   --bt-tracker="[$tracker_list]" --bt-max-peers=0 --bt-tracker-connect-timeout=300 --bt-stop-timeout=500 --min-split-size=10M \
    --follow-torrent=mem --split=10 \
    --daemon=true --allow-overwrite=true --max-overall-download-limit=$MAX_DOWNLOAD_SPEED \
    --max-overall-upload-limit=1K --max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS \
