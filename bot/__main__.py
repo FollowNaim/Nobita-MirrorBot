17eeeb8a1da6515791deae1635b8c60053b75172
NOBITA <88440061+Nobita-o@users.noreply.github.com>
2021-08-18 11:08:53 +0600

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -60,7 +60,7 @@ Type /{BotCommands.HelpCommand} to get a list of available commands
     uptime = get_readable_time((time.time() - botStartTime))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         if update.message.chat.type == "private" :
-            sendMessage(f"Hey I'm Alive 🐱\nSince: <code>{uptime}</code>", context.bot, update)
+            sendMessage(f"Hey I'm Alive 🐱\nSince: <code>{uptime}</code> \nOwner: Nobita_o", context.bot, update)
         else :
             update.effective_message.reply_photo(IMAGE_URL, start_string, parse_mode=ParseMode.MARKDOWN, reply_markup=reply_markup)
     else :
