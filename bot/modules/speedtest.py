250ea3f85329ba968542f92def07bb82e8826d66
Taki <88440061+Taki-Tachiibana@users.noreply.github.com>
2021-10-17 21:22:41 +0600

    🌞

--- bot/modules/speedtest.py
+++ bot/modules/speedtest.py
@@ -2,11 +2,10 @@ from speedtest import Speedtest
 from bot.helper.telegram_helper.filters import CustomFilters
 from bot import dispatcher, AUTHORIZED_CHATS
 from bot.helper.telegram_helper.bot_commands import BotCommands
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, ParseMode
-from telegram.ext import CallbackContext, Filters, run_async, CommandHandler
+from telegram import Update, ParseMode
+from telegram.ext import Filters, CommandHandler
 
 
-@run_async
 def speedtest(update, context):
     message = update.effective_message
     ed_msg = message.reply_text("Running Speed Test . . . ")
@@ -22,12 +21,13 @@ def speedtest(update, context):
 <b>Name:</b> <code>{result['server']['name']}</code>
 <b>Country:</b> <code>{result['server']['country']}, {result['server']['cc']}</code>
 <b>Sponsor:</b> <code>{result['server']['sponsor']}</code>
-    
+<b>ISP:</b> <code>{result['client']['isp']}</code>
+
 <b>SpeedTest Results</b>
 <b>Upload:</b> <code>{speed_convert(result['upload'] / 8)}</code>
 <b>Download:</b>  <code>{speed_convert(result['download'] / 8)}</code>
 <b>Ping:</b> <code>{result['ping']} ms</code>
-<b>ISP:</b> <code>{result['client']['isp']}</code>
+<b>ISP Rating:</b> <code>{result['client']['isprating']}</code>
 '''
     ed_msg.delete()
     try:
@@ -47,6 +47,6 @@ def speed_convert(size):
 
 
 SPEED_HANDLER = CommandHandler(BotCommands.SpeedCommand, speedtest, 
-                                                  filters=CustomFilters.authorized_user)
+                                                  filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
 
 dispatcher.add_handler(SPEED_HANDLER)
